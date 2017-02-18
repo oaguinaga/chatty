223448a288f6836d309374a41c136a6b2ff1f7f3
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-02-18 15:40:46 -0600

    Add show Chat Room functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA1FzFOmT02viCKlZplIEH2te7ajXiF1Ic",
+  authDomain: "chatty-5450d.firebaseapp.com",
+  databaseURL: "https://chatty-5450d.firebaseio.com",
+  storageBucket: "chatty-5450d.appspot.com",
+  messagingSenderId: "851650241917"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire,FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  rooms: FirebaseListObservble<any>;
+  constructor(af: AngularFire) {
+    this.rooms = af.database.list('/rooms');
+  }
+
+  ngOnInit() {
+  }
+
+}
